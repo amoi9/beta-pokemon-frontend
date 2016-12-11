b3c0e27d103055e0626f9f69d595e4dd924b54e5
amoi9 <aileenzhang7+github@gmail.com>
2016-12-10 18:07:34 -0800

    Update my_map.js

--- my_map.js
+++ my_map.js
@@ -1,6 +1,9 @@
-var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
-    credentials: 'AkXOPR73n33ySDiXHEoIiJodh58VRCLy-q6myErQF6m1ow8jzSThCI57vXwT8aFo'
-});
-var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: 'https://github.com/chenditc/mypokemon.io/blob/gh-pages/images/pushpin_images/pokemon/1.png',
-    anchor: new Microsoft.Maps.Point(12, 39) });
-map.entities.push(pushpin);
+function loadMapScenario() {
+
+    var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
+        credentials: 'AkXOPR73n33ySDiXHEoIiJodh58VRCLy-q6myErQF6m1ow8jzSThCI57vXwT8aFo'
+    });
+    var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: 'https://github.com/chenditc/mypokemon.io/blob/gh-pages/images/pushpin_images/pokemon/1.png',
+        anchor: new Microsoft.Maps.Point(12, 39) });
+    map.entities.push(pushpin);
+}
