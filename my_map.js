c19f6cf6422f081b7023041d18fb50906754c76d
amoi9 <aileenzhang7+github@gmail.com>
2016-12-10 18:21:11 -0800

    Update my_map.js

--- my_map.js
+++ my_map.js
@@ -2,10 +2,7 @@ function loadMapScenario() {
     var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
         credentials: 'AkXOPR73n33ySDiXHEoIiJodh58VRCLy-q6myErQF6m1ow8jzSThCI57vXwT8aFo'
     });
-//     var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: 'https://github.com/chenditc/mypokemon.io/blob/gh-pages/images/pushpin_images/pokemon/1.png',
-//         anchor: new Microsoft.Maps.Point(12, 39) });
-//     map.entities.push(pushpin);
-    var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: 'https://www.bingmapsportal.com/Content/images/poi_custom.png',
+    var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: 'https://github.com/chenditc/mypokemon.io/blob/gh-pages/images/pushpin_images/pokemon/1.png',
         anchor: new Microsoft.Maps.Point(12, 39) });
-    map.entities.push(pushpin)
+    map.entities.push(pushpin);
 }
