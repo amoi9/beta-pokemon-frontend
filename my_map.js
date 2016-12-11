c710e8bcca90be6205f93e94db0578b67a18650a
amoi9 <aileenzhang7+github@gmail.com>
2016-12-10 18:33:53 -0800

    Update my_map.js

--- my_map.js
+++ my_map.js
@@ -1,9 +1,33 @@
+var map_manager = {
+    "map" : null,
+    "map_items" : [],
+}
+
+map_manager.map_items = [
+    {
+        "pokemon_id": 12,
+        "expiration_timestamp_ms": 1481423394,
+        "longtitude": -73.45,
+        "latitude": 40.75,
+    },
+    {
+        "pokemon_id": 2,
+        "expiration_timestamp_ms": 1481423394,
+        "longtitude": -73.46,
+        "latitude": 40.75,
+    },
+]
+
 function loadMapScenario() {
     var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
         credentials: 'AkXOPR73n33ySDiXHEoIiJodh58VRCLy-q6myErQF6m1ow8jzSThCI57vXwT8aFo'
     });
-    var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { 
-        icon: 'https://raw.githubusercontent.com/chenditc/mypokemon.io/gh-pages/images/pushpin_images/pokemon/1.png'
-    });
-    map.entities.push(pushpin);
+    map_manager.map = map
+    
+    for (var i in map_manager.map_items) {
+        var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { 
+            icon: 'https://raw.githubusercontent.com/chenditc/mypokemon.io/gh-pages/images/pushpin_images/pokemon/' + i["pokemon_id"] +'.png'
+        });
+        map.entities.push(pushpin);
+    }
 }
