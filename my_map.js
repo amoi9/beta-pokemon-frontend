5478fe42bed18e34c86bcdf8e647bec3d6b1d4d0
amoi9 <aileenzhang7+github@gmail.com>
2016-12-10 19:41:32 -0800

    Update my_map.js

--- my_map.js
+++ my_map.js
@@ -32,7 +32,7 @@ function loadMapScenario() {
     });
     map_manager.map = map
     
-    Window.setInterval(refresh_pokemons, 1000);
+    window.setInterval(refresh_pokemons, 1000);
 }
 
 function refresh_pokemons() {
