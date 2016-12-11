8969aacbc6822080ba175440f67a78b7ce046841
amoi9 <aileenzhang7+github@gmail.com>
2016-12-10 18:35:44 -0800

    Update my_map.js

--- my_map.js
+++ my_map.js
@@ -25,8 +25,9 @@ function loadMapScenario() {
     map_manager.map = map
     
     for (var i in map_manager.map_items) {
+        map_item = map_manager.map_items[i]
         var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { 
-            icon: 'https://raw.githubusercontent.com/chenditc/mypokemon.io/gh-pages/images/pushpin_images/pokemon/' + i["pokemon_id"] +'.png'
+            icon: 'https://raw.githubusercontent.com/chenditc/mypokemon.io/gh-pages/images/pushpin_images/pokemon/' + map_item["pokemon_id"] +'.png'
         });
         map.entities.push(pushpin);
     }
