3c0f4b11c72f3fb961b592be208f609528acbfc0
amoi9 <aileenzhang7+github@gmail.com>
2016-12-10 17:55:15 -0800

    Create my_map.js

---
+++ my_map.js
@@ -0,0 +1,6 @@
+var map;
+function loadMapScenario() {
+  map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
+      credentials: 'AkXOPR73n33ySDiXHEoIiJodh58VRCLy-q6myErQF6m1ow8jzSThCI57vXwT8aFo'
+  });
+}
